ae6f5dc28674b311077060cdf0f99f7913d11e19
Govanni González Rivera <103330976+GoRiDeveloper@users.noreply.github.com>
2023-10-10 23:25:54 -0600

    Revert "Cloudinary service"

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { join } from 'node:path';
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { CouponsModule } from './coupons/coupons.module';
-import { AuthModule } from './auth/auth.module';
-import { GoogleStrategy } from './google auth/google.strategy';
-import { User, UserSchema } from './users/entities/user.entity';
-import { CloudinaryModule } from './cloudinary/cloudinary.module';
-=======
-=======
->>>>>>> ceeab4628e1f4b8721d63036be1d74673d1837a9
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
@@ -30,10 +11,6 @@ import { CouponsModule } from './coupons/coupons.module'
 import { AuthModule } from './auth/auth.module'
 import { GoogleStrategy } from './google auth/google.strategy'
 import { User, UserSchema } from './users/entities/user.entity'
-<<<<<<< HEAD
->>>>>>> ceeab4628e1f4b8721d63036be1d74673d1837a9
-=======
->>>>>>> ceeab4628e1f4b8721d63036be1d74673d1837a9
 
 @Module({
   imports: [
@@ -50,16 +27,7 @@ import { User, UserSchema } from './users/entities/user.entity'
     }),
     UsersModule,
     CouponsModule,
-<<<<<<< HEAD
-<<<<<<< HEAD
-    AuthModule,
-    CloudinaryModule
-=======
     AuthModule
->>>>>>> ceeab4628e1f4b8721d63036be1d74673d1837a9
-=======
-    AuthModule
->>>>>>> ceeab4628e1f4b8721d63036be1d74673d1837a9
   ],
   controllers: [AppController],
   providers: [AppService, GoogleStrategy]
