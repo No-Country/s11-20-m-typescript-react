7606beaf67d3484a0afe72ec2dd4dc8d59d4a977
Thomas Barenghi <90881270+thomasbarenghi@users.noreply.github.com>
2023-10-25 14:44:08 -0300

    feat: ts standart

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,51 +1,35 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json'
   },
-  'extends': [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/jsx-runtime'
-  ],
-  'overrides': [
-    {
-      'env': {
-        'node': true
-      },
-      'files': [
-        '.eslintrc.{tsx,js,cjs}'
-      ],
-      'parserOptions': {
-        'sourceType': 'script'
+  env: {
+    browser: true,
+    es2021: true
+  },
+  plugins: ['prefer-arrow', '@typescript-eslint/eslint-plugin', 'prettier'],
+  extends: ["standard-jsx", 'standard-with-typescript'],
+  ignorePatterns: ['.eslintrc.cjs', "node_modules/"],
+  rules: {
+    'arrow-body-style': 'error',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'prefer-arrow-callback': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'prefer-arrow/prefer-arrow-functions': [
+      'warn',
+      {
+        disallowPrototype: true,
+        singleReturnOnly: false,
+        classPropertiesAllowed: false
       }
-    }
-  ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
-  },
-  'plugins': [
-    '@typescript-eslint',
-    'react'
-  ],
-  'rules': {
-    'indent': [
-      'error',
-      2
     ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'never'
-    ]
+    'multiline-ternary': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-misused-promises': 'off',
+    '@typescript-eslint/no-floating-promises': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off',
+    "@typescript-eslint/strict-boolean-expressions": "off",
   }
 }
